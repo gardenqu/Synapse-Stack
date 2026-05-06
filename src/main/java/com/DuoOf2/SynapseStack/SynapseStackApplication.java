fb7cf97003b60770fcb3209cf89ff84807d06334
Quanayzia Garden <qjgarden1@gmail.com>
2026-05-05 21:03:46 -0400

    Initial commit

---
+++ src/main/java/com/DuoOf2/SynapseStack/SynapseStackApplication.java
@@ -0,0 +1,13 @@
+package com.DuoOf2.SynapseStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapseStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapseStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DuoOf2/SynapseStack/SynapseStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DuoOf2.SynapseStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
